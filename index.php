29b1060fd909e911d20638635e959a2701c21cff
Gustavo Êrades Vilarinho Silva <108373134+Gustavo-erades@users.noreply.github.com>
2024-01-06 02:18:00 -0300

    Update index.php

--- index.php
+++ index.php
@@ -19,14 +19,11 @@
     <div class="container">
         <div class="texto">
             <h1>Olá, me chamo Mel!</h1>
+            <hr>
             <p>
                 Caso você tenha me encontrado, por favor, entre em contato com meus donos. Eles estão a minha procura!
-                Você pode <a target="_blank" href="mailto:meuemail@gmail.com?subject=Achei a mel">enviar um email</a> para eles, <a target="_blank" href="https://api.whatsapp.com/send?phone=5561999999999&text=Achei%20a%20Mel!">mandar mensagem</a> ou até mesmo <a href="tel:+5561999999999">ligar</a> para eles. Muito obrigada.
-            </p>
-        </div>
-        <div>
-            <p>
-                Quanto a meus donos você pode procurar por:
+                Você pode <a target="_blank" href="mailto:eradesvilarinho@gmail.com?subject=Achei a mel">enviar um email</a> para eles, <a target="_blank" href="https://api.whatsapp.com/send?phone=5561994490664&text=Achei%20a%20Mel!">mandar mensagem</a> ou até mesmo <a href="tel:+5561994490664">ligar</a> para eles.
+                Quanto a meus donos procure por:
                 <ul>
                     <li>André Luiz</li>
                     <li>Késsia Moreira</li>
@@ -35,7 +32,7 @@
                 </ul>
             </p>
             <p>
-                Se puder, ligue lá em casa usando o número <a href="tel:+5561999999999">(61) 3256-6605</a>
+                Se puder, ligue lá em casa usando o número <a href="tel:+556132566605">(61) 3256-6605</a>, obrigada &#128522;
             </p>
         </div>
         <div>
